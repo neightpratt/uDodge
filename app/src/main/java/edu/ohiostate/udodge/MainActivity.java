a91f00cf9feb3f7137e19d3f1b79292dd941d410
neightpratt <pratt.251@buckeyemail.osu.edu>
2018-02-18 18:41:48 -0500

    added sendMessage() to MainActivity

--- app/src/main/java/edu/ohiostate/udodge/MainActivity.java
+++ app/src/main/java/edu/ohiostate/udodge/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.ohiostate.udodge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +10,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,10 +31,7 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // replace this shit
-                Toast.makeText(MainActivity.this,
-                        R.string.play,
-                        Toast.LENGTH_SHORT).show();
+                sendMessage(v);
             }
         });
 
@@ -64,5 +65,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, null/*PlayActivity.class*/);
+        startActivity(intent);
     }
 }
