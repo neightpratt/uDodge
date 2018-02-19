944d1be3fde45d7ae03fbe2ef0d1bf3342dfe6f7
nlamantia <lamantia.27@buckeyemail.osu.edu>
2018-02-18 19:42:48 -0500

    Implemented basic score increment every time the user touches ben stiller.

--- app/src/main/java/edu/ohiostate/udodge/PlayActivity.java
+++ app/src/main/java/edu/ohiostate/udodge/PlayActivity.java
@@ -2,12 +2,40 @@ package edu.ohiostate.udodge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
+
+    // get a hold of the character
+    private ImageView mCharacter;
+    private TextView mScoreLabel;
+
+    // score
+    private int score = 0;
+
+    public PlayActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        mCharacter = (ImageView) findViewById(R.id.characterView);
+        mScoreLabel = (TextView) findViewById(R.id.score_label);
+        mScoreLabel.setText("" + score);
+
+        mCharacter.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent m) {
+                score++;
+                mScoreLabel.setText("" + score);
+                return true;
+            }
+        });
+
     }
 }
