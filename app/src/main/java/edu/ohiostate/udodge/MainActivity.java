8f3ef13896eb17b6023cec2991209c92f68121d9
neightpratt <pratt.251@buckeyemail.osu.edu>
2018-02-18 19:11:45 -0500

    added log msgs to oncreate in play activity and main activity

--- app/src/main/java/edu/ohiostate/udodge/MainActivity.java
+++ app/src/main/java/edu/ohiostate/udodge/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,9 +19,11 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class MainActivity extends AppCompatActivity {
 
     private Button mPlayButton;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "MainActivity onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -69,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, null/*PlayActivity.class*/);
+        Intent intent = new Intent(this, PlayActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/edu/ohiostate/udodge/PlayActivity.java
+++ app/src/main/java/edu/ohiostate/udodge/PlayActivity.java
@@ -2,11 +2,14 @@ package edu.ohiostate.udodge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class PlayActivity extends AppCompatActivity {
 
+    private static final String TAG = "PlayActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "PlayActivity onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
     }
