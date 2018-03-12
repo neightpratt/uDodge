fcba8ae549dbe410873ad004582eb5c8332e9d1f
96gkuver <gskuver@comcast.net>
2018-03-12 13:25:28 -0400

    Created one main Fragment Activity and changed our other two activities to just set what fragment is created

--- app/src/main/java/edu/ohiostate/udodge/MainActivity.java
+++ app/src/main/java/edu/ohiostate/udodge/MainActivity.java
@@ -1,60 +1,16 @@
 package edu.ohiostate.udodge;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
+public class MainActivity extends SingleFragmentActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate(Bundle) called");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.main_fragment_container);
-
-        if (fragment == null){
-            fragment = new MainFragment();
-            fm.beginTransaction().add(R.id.main_fragment_container, fragment).commit();
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
+    protected Fragment createFragment(){
+        return new MainFragment();
     }
 }
--- app/src/main/java/edu/ohiostate/udodge/PlayActivity.java
+++ app/src/main/java/edu/ohiostate/udodge/PlayActivity.java
@@ -10,51 +10,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PlayActivity extends AppCompatActivity {
-
-    private static final String TAG = "PlayActivity";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate(Bundle) called");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.play_fragment_container);
-
-        if (fragment == null){
-            fragment = new PlayFragment();
-            fm.beginTransaction().add(R.id.play_fragment_container, fragment).commit();
-        }
-    }
+public class PlayActivity extends SingleFragmentActivity{
 
     @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
+    protected Fragment createFragment(){
+        return new PlayFragment();
     }
 }
