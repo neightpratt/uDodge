1aded42f07089d424a588bc8ab453ddcfaf5380b
96gkuver <gskuver@comcast.net>
2018-03-12 13:46:30 -0400

    Created one main Fragment Activity and changed our other two activities to just set what fragment is created

---
+++ app/src/main/java/edu/ohiostate/udodge/Score.java
@@ -0,0 +1,8 @@
+package edu.ohiostate.udodge;
+
+/**
+ * Created by Greg on 3/12/2018.
+ */
+
+public class Score {
+}
---
+++ app/src/main/java/edu/ohiostate/udodge/SingleFragmentActivity.java
@@ -0,0 +1,28 @@
+package edu.ohiostate.udodge;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Greg on 3/12/2018.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
